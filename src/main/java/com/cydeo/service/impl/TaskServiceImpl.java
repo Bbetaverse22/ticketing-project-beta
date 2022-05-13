1464a6d3323e8b2726e375350fb30a5e284f9993
BetulB <betul.bogrek@gmail.com>
2022-05-12 20:30:47 -0500

    tasks parts are added

---
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -0,0 +1,83 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.entity.Task;
+import com.cydeo.entity.User;
+import com.cydeo.enums.Status;
+import com.cydeo.mapper.TaskMapper;
+import com.cydeo.repository.TaskRepository;
+import com.cydeo.service.TaskService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+    private final TaskMapper taskMapper;
+
+    public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper) {
+        this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
+    }
+
+    @Override
+    public TaskDTO findById(Long id) {
+        Optional<Task> task = taskRepository.findById(id);
+        if(task.isPresent()){
+            return taskMapper.convertToDto(task.get());
+        }
+        return null;
+    }
+
+    @Override
+    public List<TaskDTO> listAllTasks() {
+        return taskRepository.findAll().stream().map(taskMapper::convertToDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public void save(TaskDTO dto) {
+        dto.setTaskStatus(Status.OPEN);
+        Task task = taskMapper.convertToEntity(dto);
+        taskRepository.save(task);
+    }
+
+    @Override
+    public TaskDTO update(TaskDTO dto) {
+        Optional<Task> task = taskRepository.findById(dto.getId());
+
+        Task convertedTask = taskMapper.convertToEntity(dto);
+
+        if(task.isPresent()){
+            convertedTask.setId(task.get().getId());
+            convertedTask.setTaskStatus(task.get().getTaskStatus());
+            convertedTask.setAssignedDate(task.get().getAssignedDate());
+            taskRepository.save(convertedTask);
+        }
+
+        return findById(dto.getId());
+    }
+
+    @Override
+    public void delete(Long id) {
+
+        Optional<Task> foundTask = taskRepository.findById(id);
+        if(foundTask.isPresent()){
+            foundTask.get().setIsDeleted(true);
+            taskRepository.save(foundTask.get());
+        }
+    }
+
+    @Override
+    public int totalNonCompletedTask(String projectCode) {
+        return taskRepository.totalNonCompletedTasks(projectCode);
+    }
+
+    @Override
+    public int totalCompletedTask(String projectCode) {
+        return taskRepository.totalCompletedTasks(projectCode);
+    }
+}
